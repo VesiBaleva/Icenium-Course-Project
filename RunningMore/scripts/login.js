acda36574cce8431920b44c69fc3afbed34b5e27
Veselina Baleva <vbaleva@yahoo.com>
2013-10-13 06:53:40 -0500

    Login works

--- RunningMore/scripts/login.js
+++ RunningMore/scripts/login.js
@@ -1,43 +1,67 @@
-(function (global) {
-    var LoginViewModel,
-        app = global.app = global.app || {};
+var app = app || {};
+var el = new Everlive({
+		apiKey: 'e93vqIuZZyQeoiKg'
+	});
 
-    LoginViewModel = kendo.data.ObservableObject.extend({
+(function (a) {   
+    
+    var viewModel = kendo.observable({
         isLoggedIn: false,
         username: "",
         password: "",
-
-        onLogin: function () {
-            var that = this,
-                username = that.get("username").trim(),
-                password = that.get("password").trim();
-
-            if (username === "" || password === "") {
-                navigator.notification.alert("Both fields are required!",
-                    function () { }, "Login failed", 'OK');
-
-                return;
-            }
-
-            that.set("isLoggedIn", true);
-        },
-
-        onLogout: function () {
-            var that = this;
-
-            that.clearForm();
-            that.set("isLoggedIn", false);
-        },
-
-        clearForm: function () {
-            var that = this;
-
-            that.set("username", "");
-            that.set("password", "");
-        }
+        login:onLogin,
+        logout:onLogout,
+        clearForm:clearForm
+        
     });
+    
+    function init(e) {
+        kendo.bind(e.view.element, viewModel);
+    }
+    
+    function onLogin() {
+        
+       // var userModel=new usersModel();
+        username = $('#loginUsername').val();
+		password = $('#loginPassword').val();
 
-    app.loginService = {
-        viewModel: new LoginViewModel()
+        if (username === "" || password === "") {
+            navigator.notification.alert("Both fields are required!",
+                function () { }, "Login failed", 'OK');
+
+            return;
+        }
+        
+        el.Users.login(username, password)
+		.then(function () {
+            
+			return usersModel.load();
+            
+		})
+		.then(function () {
+			app.aplication.navigate('views/activitiesView.html');
+            viewModel.set("isLoggedIn", true);
+		})
+		.then(null,
+			  function (err) {
+				   navigator.notification.alert("Error:"+err.message)
+			  }
+		);
+    }
+    
+    function onLogout() {
+       
+
+          //  that.clearForm();
+            viewModel.set("isLoggedIn", false);
+    }
+    
+    function clearForm () {
+        viewModel.set("username", "");
+        viewModel.set("password", "");
+    }
+    
+    a.loginService = {
+        init: init
     };
-})(window);
+}(app));
